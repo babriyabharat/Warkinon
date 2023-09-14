29debe726140ba48a0e89ddb41f58ed5d14db0a5
babriyabharat <bharatbabriya2005@gmail.com>
2023-09-14 17:48:17 +0530

    second-commit

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -21,6 +21,7 @@ import Portfolio2 from '../Image/Portfolio2.png';
 import Portfolio3 from '../Image/Portfolio3.png';
 import Portfolio4 from '../Image/Portfolio4.png';
 import Testimonial from '../Image/Testimonial.png';
+import { Facebook, Instagram, Twitter } from "react-bootstrap-icons";
 
 function Home() {
     const settings = {
@@ -36,28 +37,8 @@ function Home() {
     };
     return (
         <>
-            {/* <section className='header '>
-                <div className='container header-menu d-flex'>
-                    <Navbar expand="lg" className="bg-body-tertiary">
-                        <Navbar.Brand href="#home"><img src={Logo} /></Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" className='text-end'>
-                            <Nav className="menu me-auto">
-                                <Nav.Link href="#home" className='fs-6 a'>Home</Nav.Link>
-                                <Nav.Link href="#About" className='fs-6 a'>About Us</Nav.Link>
-                                <Nav.Link href="#Service" className='fs-6 a'>Service</Nav.Link>
-                                <Nav.Link href="#Project" className='fs-6 a'>Project</Nav.Link>
-                                <Nav.Link href="#Contact" className='fs-6 a'>Contact Us</Nav.Link>
-                            </Nav>
-                            <div className='header-btn'>
-
-                            </div>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </div>
-            </section> */}
             <section className="header">
-                <div className="container header-menu">
+                <div className="container header-menu d-flex">
                     <Navbar>
                         <Navbar.Brand href="#home"><img src={Logo} alt="logo" /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -143,7 +124,7 @@ function Home() {
                         <h1>I want company feature</h1>
                         <p>We believe scaling startups require a radically different kind of agency</p>
                     </div>
-                    <div className="feature-image d-flex">
+                    <div className="feature-image d-flex text-center">
                         <div className="img col-4">
                             <img src={Feature1} alt="Feature1" />
                         </div>
@@ -163,7 +144,7 @@ function Home() {
                         <p>We recruit doctors with the best honors and graduates at their<br /> universities, with a very broad and extraordinary experience<br /> they grow into professional doctors</p>
                         <p>then received extraordinary supplies, guided for 2 years, they<br /> already understand very well about existing diseases and how<br /> to handle them properly</p>
                     </div>
-                    <div className="Bussiness-image col-6">
+                    <div className="bussiness-image col-6">
                         <img src={Bussiness} alt="Bussiness" />
                     </div>
                 </div>
@@ -200,15 +181,102 @@ function Home() {
                         <h1>Our Testimonial</h1>
                         <p>We believe scaling startups require a radically different kind of agency</p>
                     </div>
-                    <div className="testimonial-image d-flex">
+                    <div className="testimonial-image d-flex text-center">
                         <div className="img col-4">
-                            <img src={Testimonial} alt="Testimonial"/>
+                            <img src={Testimonial} alt="Testimonial" />
                         </div>
                         <div className="img col-4">
-                            <img src={Testimonial} alt="Testimonial"/>
+                            <img src={Testimonial} alt="Testimonial" />
                         </div>
                         <div className="img col-4">
-                            <img src={Testimonial} alt="Testimonial"/>
+                            <img src={Testimonial} alt="Testimonial" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="contact">
+                <div className="container">
+                    <div className="contact-info text-center">
+                        <p>WE CUSTOM BUILD WEBSITES TO MAKE YOUR BRAND STAND OUT FROM THE NOISE</p>
+                        <h1>We start by listening and let that shape our<br />design process, with our clients as partners</h1>
+                        <div>
+                            <button type="button" class="btn">Contact Us</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="footer">
+                <div className="container">
+                    <div className="footer-top d-flex">
+                        <div className="footer-top-menu col-6">
+                            <ul className="d-flex">
+                                <li><a>About</a></li>
+                                <li><a>Service</a></li>
+                                <li><a>Pricing plans</a></li>
+                                <li><a>Privacy</a></li>
+                                <li><a>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-top-icon col-6 text-end d-flex">
+                            <div className="icon">
+                                <Facebook />
+                            </div>
+                            <div className="icon">
+                                <Instagram />
+                            </div>
+                            <div className="icon">
+                                <Twitter />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-main d-flex">
+                        <div className="footer-main-menu col-2">
+                            <ul className="">
+                                <li><a>About</a></li>
+                                <li><a>Service</a></li>
+                                <li><a>Pricing plans</a></li>
+                                <li><a>Privacy</a></li>
+                                <li><a>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-main-menu col-2">
+                            <ul className="">
+                                <li><a>About</a></li>
+                                <li><a>Service</a></li>
+                                <li><a>Pricing plans</a></li>
+                                <li><a>Privacy</a></li>
+                                <li><a>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-main-menu col-2">
+                            <ul className="">
+                                <li><a>About</a></li>
+                                <li><a>Service</a></li>
+                                <li><a>Pricing plans</a></li>
+                                <li><a>Privacy</a></li>
+                                <li><a>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-main-menu col-2">
+                            <ul className="">
+                                <li><a>About</a></li>
+                                <li><a>Service</a></li>
+                                <li><a>Pricing plans</a></li>
+                                <li><a>Privacy</a></li>
+                                <li><a>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-main-contact col-4 ">
+                            <h5>Contact</h5>
+                            <p>New York. 112 W<br />34th St, Manhattan<br />(1) 212-445-4320</p>
+                        </div>
+                    </div>
+                    <div className="footer-end d-flex">
+                        <div className="footer-end-contant col-6">
+                            <p>Sways by keydesign. All Rights Reserved</p>
+                        </div>
+                        <div className="footer-end-contant col-6">
+                            <p>Terms of Use Privacy Policy</p>
                         </div>
                     </div>
                 </div>
